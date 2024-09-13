4c2ff3bf19d121ba9d01594fa9af5e7ecafde8ab
kosty <kosty@DESKTOP-UECDAL7>
2024-09-13 19:37:52 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+	int arr[] = { 2,8,16 };
+	for (int i = 0; i < 3; i++)
+		cout << arr[i] <<" ";
+	//int n;
+	//cin >> n;
+	//for (int i = 1; i <= n; i += 1) {
+	//	cout << "Счетчик равен " << i << endl;
+	return 0;
+}
+	
